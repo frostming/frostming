9e37f6d2a7a0170963117bffb12042f314cc1a8e
Frost Ming <mianghong@gmail.com>
2021-03-26 23:17:08 +0800

    Add surrender button

--- chess/runner.py
+++ chess/runner.py
@@ -1,30 +1,34 @@
 import json
 import os
 import re
+from pathlib import Path
 from subprocess import check_call
 from typing import Literal, Union
 
+from github3 import login
 from jinja2 import Template
 
 from chess.gomoku import Game
 
+BASE_DIR = Path(__file__).parent
+
+
+def print_title(title: str):
+    print("=" * 8 + " " + title + " " + "=" * 8)
+
 
 class Runner:
-    GAME_META = os.path.join(os.path.dirname(__file__), "gomoku.json")
-    GAME_STATS = os.path.join(os.path.dirname(__file__), "stats.json")
-    README = os.path.join(os.path.dirname(os.path.dirname(__file__)), "README.md")
+    GAME_META = BASE_DIR / "gomoku.json"
+    GAME_STATS = BASE_DIR / "stats.json"
+    README = BASE_DIR.parent / "README.md"
     ROLES = {0: "white", 1: "black"}
-    TEMPLATE = os.path.join(os.path.dirname(__file__), "readme_template.j2")
+    TEMPLATE = BASE_DIR / "templates/readme.j2"
+    SUMMARY_TEMPLATE = BASE_DIR / "templates/summary.j2"
 
     def __init__(self) -> None:
-        with open(self.GAME_META, encoding="utf-8") as f:
-            self.meta = json.load(f)
-        with open(self.GAME_STATS, encoding="utf-8") as f:
-            self.stats = json.load(f)
-        with open(self.README, encoding="utf-8") as f:
-            self.readme = f.read()
-        with open(self.TEMPLATE, encoding="utf-8") as f:
-            self.template = Template(f.read())
+        self.meta = json.load(self.GAME_META.open(encoding="utf-8"))
+        self.stats = json.load(self.GAME_STATS.open(encoding="utf-8"))
+        self.readme = self.README.read_text(encoding="utf-8")
 
     def play_game(
         self, role: Union[Literal[0], Literal[1]], pos: str, user: str
@@ -47,29 +51,24 @@ class Runner:
         self.meta["blacks"], self.meta["whites"] = game.dump()
         winner = game.check_field(idx)
         if winner is not None:
-            self.meta["winner"] = winner
-            self.stats["completed_games"] += 1
-            if winner == role:
-                self.stats["winning_players"][user] = (
-                    self.stats["winning_players"].get(user, 0) + 1
-                )
+            self.end_game(winner, user)
         else:
             self.meta["role"] ^= 1
 
     def dump(self):
-        with open(self.GAME_META, "w", encoding="utf-8") as f:
-            json.dump(self.meta, f, indent=2)
-        with open(self.GAME_STATS, "w", encoding="utf-8") as f:
-            json.dump(self.stats, f, indent=2)
-        with open(self.README, "w", encoding="utf-8") as f:
-            f.write(self.readme)
+        print_title("dumping data")
+        json.dump(self.meta, self.GAME_META.open("w", encoding="utf-8"))
+        json.dump(self.stats, self.GAME_STATS.open("w", encoding="utf-8"))
+        self.README.write_text(self.readme, "utf-8")
 
     def new_game(self):
+        print_title("initializing game")
         self.meta.update(
             turn=0, last_steps=[], blacks=[], whites=[], role=1, winner=None
         )
 
     def update_readme(self) -> None:
+        print_title("Updating README")
         game = Game()
         game.load(self.meta["blacks"], self.meta["whites"])
         top_wins = sorted(
@@ -88,9 +87,10 @@ class Runner:
             "top_wins": top_wins,
             "repo": os.getenv("REPO"),
         }
+        template = Template(self.TEMPLATE.read_text("utf-8"))
         self.readme = re.sub(
             r"(<\!--START_SECTION:gomoku-->\n)(.*)(\n<\!--END_SECTION:gomoku-->)",
-            r"\1" + self.template.render(**context) + r"\3",
+            r"\1" + template.render(**context) + r"\3",
             self.readme,
             flags=re.DOTALL,
         )
@@ -99,29 +99,66 @@ class Runner:
         check_call(["git", "add", "-A", "."])
         check_call(["git", "commit", "-m", message])
 
+    def end_game(self, win_role: int, winner: str):
+        print_title("end of game")
+        self.meta["winner"] = win_role
+        self.stats["completed_games"] += 1
+        self.stats["winning_players"][winner] = (
+            self.stats["winning_players"].get(winner, 0) + 1
+        )
+        game = Game()
+        game.load(self.meta["blacks"], self.meta["whites"])
+        template = Template(self.SUMMARY_TEMPLATE.read_text("utf-8"))
+        context = {
+            "dimension": game.DIMENSION,
+            "field": game.field(),
+            "color": self.ROLES.get(win_role),
+            "winner": winner,
+            "last_steps": self.meta["last_steps"],
+        }
+        summary_text = template.render(**context)
+        gh = login(token=os.getenv("GITHUB_TOKEN"))
+        namespace, repo = os.getenv("REPO").split("/")
+        issue_num = int(os.getenv("ISSUE_NUMBER"))
+        issue = gh.issue(namespace, repo, issue_num)
+        issue.create_comment(summary_text)
 
-def main():
-    issue_title = os.getenv("ISSUE_TITLE")
-    parts = issue_title.split("|")
-    runner = Runner()
-    user = os.getenv("PLAYER")
-    if len(parts) < 4 and "new" in parts:
-        runner.new_game()
-        message = f"@{user} starts a new game"
-    else:
-        *_, color, pos = parts
-        input_role = 1 if color == "black" else 0
-        runner.play_game(input_role, pos, user)
-        message = f"@{user} drops a {color} piece at {pos.upper()}"
-    message += f"\nClose #{os.getenv('ISSUE_NUMBER')}"
-    runner.update_readme()
-    runner.dump()
-    runner.commit_files(message)
+    def main(self):
+        issue_title = os.getenv("ISSUE_TITLE")
+        parts = issue_title.split("|")
+        user = os.getenv("PLAYER")
+        if len(parts) < 4 and "new" in parts:
+            print_title("new game")
+            self.new_game()
+            message = f"@{user} starts a new game"
+        elif len(parts) < 4 and "surrender" in parts:
+            print_title("surrender")
+            try:
+                last_step = self.meta["last_steps"][0]
+            except IndexError:
+                last_step = None
+            if last_step is not None:
+                winner = last_step["user"]
+                color = last_step["color"]
+                role = 1 if color == "black" else "white"
+                self.end_game(role, winner)
+            self.new_game()
+            message = (
+                f"@{user} surrenders and @{last_step['user']} wins"
+                if last_step
+                else f"@{user} surrenders"
+            )
+        else:
+            print_title("perform drop")
+            *_, color, pos = parts
+            input_role = 1 if color == "black" else 0
+            self.play_game(input_role, pos, user)
+            message = f"@{user} drops a {color} piece at {pos.upper()}"
+        message += f"\nClose #{os.getenv('ISSUE_NUMBER')}"
+        self.update_readme()
+        self.dump()
+        self.commit_files(message)
 
 
 if __name__ == "__main__":
-    main()
-    # os.environ["REPO"] = "frostming/frostming"
-    # runner = Runner()
-    # runner.update_readme()
-    # runner.dump()
+    Runner().main()
